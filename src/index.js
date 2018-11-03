6d3d3454db84dc138a5f20391f738aaa55ff216b
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-11-03 05:39:09 +0530

    feat: remove starter code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const App = () => (
+    <div>YO</div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
