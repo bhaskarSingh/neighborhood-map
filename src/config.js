280c2a56999dc60c5269d9e1f4f0c5e2c463640f
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-11-05 11:38:10 +0530

    feat: Add config file

---
+++ src/config.js
@@ -0,0 +1,9 @@
+/**
+|--------------------------------------------------
+| ADD YOUR OWN API KEY HERE
+|--------------------------------------------------
+*/
+
+export const GOOGLE_MAPS_API_KEY = '';
+export const SQUARESPACE_CLIENT_ID = '';
+export const SQUARESPACE_CLIENT_SECRET = '';
